d4f6b1aa8abeac3e0ff58890762a574016df8964
michael-gubi4 <michail.hubich@specific-group.by>
2014-11-12 18:00:47 +0300

    EMO-37

--- Enquire/EnquireWebNew/web/js/question-add-helper.js
+++ Enquire/EnquireWebNew/web/js/question-add-helper.js
@@ -1,21 +1,21 @@
 $(window).load(function(){
     var questions = [], existingQuestions = [];
-    var $questionary = $('#form-reihenfolge').val();
     var $modal = $('#myModal');
 
     function applyQuestionsAdding(){
+        var $questionary = $('#form-reihenfolge').val();
         existingQuestions = getExisting($questionary).concat(questions);
         $('.question-add.btn-warning').each(function(){
             var $questionId = $(this).data('question');
             if(existingQuestions.indexOf($questionId)!==-1){
                 $(this).removeClass('btn-warning');
+            }else{
+                $(this).removeClass('btn-warning').addClass('btn-warning');
             }
         });
         $('.question-add.btn-warning').on('click', function(){
-            var $text = $(this).text();
-            //$(this).text($text == "Add" ? "Del" : "Add");
-            $(this).removeClass('btn-warning').off('click');
             var $questionId = $(this).data('question');
+            $(this).removeClass('btn-warning').off('click');
             questions.push($questionId);
         });
     }
@@ -43,8 +43,29 @@ $(window).load(function(){
         questions = [];
     });
 
+    $('#addQuestion').on('click', function (e) {
+        var $questionary = $('#form-reihenfolge').val();
+        existingQuestions = getExisting($questionary).concat(questions);
+        $('.question-add').each(function(){
+            var $questionId = $(this).data('question');
+            if(existingQuestions.indexOf($questionId)!==-1){
+                $(this).removeClass('btn-warning');
+                $(this).off('click');
+            }else{
+                if(!$(this).hasClass('btn-warning')){
+                    $(this).addClass('btn-warning');
+                    $(this).on('click', function(){
+                        $(this).removeClass('btn-warning').off('click');
+                        questions.push($questionId);
+                    });
+                }
+            }
+        });
+    });
+
     $('#reinfolge-save').bind('click', function(){
         if(questions.length != 0){
+            var $questionary = $('#form-reihenfolge').val();
             $questionary = $questionary + questions.join(';') + ';';
             $('#form-reihenfolge').val($questionary);
         }
