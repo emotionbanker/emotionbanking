9db5250e661c0b3e11c529c1d0198504d5c2bb7b
michael-gubi4 <michail.hubich@specific-group.by>
2014-11-13 13:44:11 +0300

    EMO-36

--- Enquire/EnquireWebNew/views/bank/statistic-list.php
+++ Enquire/EnquireWebNew/views/bank/statistic-list.php
@@ -10,53 +10,63 @@ $this->title = 'Statistik';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="bank-view">
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        <?php foreach ($list as $bank): ?>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingOne">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $bank->b_id;?>" aria-expanded="false" aria-controls="collapseOne">
+                        <?php echo $bank->bezeichnung; ?><br/>
+                    </a>
+                </h4>
+            </div>
+            <div id="collapse<?php echo $bank->b_id;?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                    <h3><?php echo $bank->bezeichnung?></h3>
+                    <?php
 
-	<?php foreach ($list as $bank): ?>
-		<?php echo Html::a($bank->bezeichnung, '#' . $bank->b_id) ?><br/>
-	<?php endforeach;?>
-
-	<?php foreach ($list as $bank): ?>
-		<h3 id="<?php echo  $bank->b_id?>"><?php echo $bank->bezeichnung?></h3>
-		<?php
-
-		$searchModel = new \app\models\search\Statistic($bank->b_id);
-		$dataProvider = $searchModel->search();
-		?>
-		<?= GridView::widget([
-			'dataProvider' => $dataProvider,
-			'columns' => [
-				[
-					'label' => 'Personengruppe',
-					'attribute' => 'bezeichnung',
-					'width' => '20%'
-				],
-				[
-					'label' => 'Anz. Codes',
-					'attribute' => 'tic_count',
-					'pageSummary' => true,
-					'width' => '20%'
-				],
-				[
-					'label' => 'Komplett',
-					'attribute' => 'bused',
-					'pageSummary' => true,
-					'width' => '20%'
-				],
-				[
-					'label' => 'nur 50 oder mehr',
-					'attribute' => 'bused50',
-					'pageSummary' => true,
-					'width' => '20%'
-				],
-				[
-					'label' => 'Gesamt in Auswertung',
-					'attribute' => 'busedtot',
-					'pageSummary' => true,
-					'width' => '20%'
-				],
-			],
-			'showPageSummary' => true
-		]);
-		?>
-	<?php endforeach;?>
+                    $searchModel = new \app\models\search\Statistic($bank->b_id);
+                    $dataProvider = $searchModel->search();
+                    ?>
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'columns' => [
+                            [
+                                'label' => 'Personengruppe',
+                                'attribute' => 'bezeichnung',
+                                'width' => '20%'
+                            ],
+                            [
+                                'label' => 'Anz. Codes',
+                                'attribute' => 'tic_count',
+                                'pageSummary' => true,
+                                'width' => '20%'
+                            ],
+                            [
+                                'label' => 'Komplett',
+                                'attribute' => 'bused',
+                                'pageSummary' => true,
+                                'width' => '20%'
+                            ],
+                            [
+                                'label' => 'nur 50 oder mehr',
+                                'attribute' => 'bused50',
+                                'pageSummary' => true,
+                                'width' => '20%'
+                            ],
+                            [
+                                'label' => 'Gesamt in Auswertung',
+                                'attribute' => 'busedtot',
+                                'pageSummary' => true,
+                                'width' => '20%'
+                            ],
+                        ],
+                        'showPageSummary' => true
+                    ]);
+                    ?>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
 </div>
